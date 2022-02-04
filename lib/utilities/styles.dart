3cada51709dd1f2a07661520b4a53b9acda84ece
EthanMayer <ethan.h.mayer@vanderbilt.edu>
2022-02-04 13:03:00 -0600

    Added styles folder to standardize text and color styles per school

---
+++ lib/utilities/styles.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+
+/// Class to hold all of the custom styles.
+abstract class VanderbiltStyles {
+  // Official Vanderbilt gold.
+  static const Color gold = Color.fromRGBO(216, 171, 76, 1);
+
+  // Official dark Vanderbilt gold.
+  static const Color darkerGold = Color.fromRGBO(153, 127, 61, 1);
+
+  // Standardized Apple blue used for nav bar buttons.
+  static const Color systemBlue = CupertinoColors.systemBlue;
+
+  // Standardized placeholder text style.
+  static const TextStyle textRowPlaceholder = TextStyle(
+      color: CupertinoColors.systemGrey,
+      fontWeight: FontWeight.bold,
+      fontSize: 20
+  );
+
+  // Standardized text style.
+  static const TextStyle textRow = TextStyle(
+      color: CupertinoColors.black,
+      fontSize: 20
+  );
+
+  // Standardized button text style.
+  static const TextStyle textButton = TextStyle(
+      color: CupertinoColors.white,
+      fontSize: 25,
+      fontWeight: FontWeight.bold,
+  );
+}
