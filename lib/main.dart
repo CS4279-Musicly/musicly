29d0b969a42297bc47a28775dff1eb56a99e104b
EthanMayer <ethan.h.mayer@vanderbilt.edu>
2022-02-07 12:39:30 -0600

    Added login page with title and text box

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:musicly/utilities/styles.dart';
 
 // Starts the app
 void main() => runApp(Musicly());
@@ -7,7 +8,7 @@ void main() => runApp(Musicly());
 class Musicly extends StatelessWidget {
   const Musicly({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  // Root of the application
   @override
   Widget build(BuildContext context) {
     // Cupertino App is the iOS-style app flutter offers
@@ -16,42 +17,21 @@ class Musicly extends StatelessWidget {
       theme: CupertinoThemeData(
         brightness: Brightness.dark,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginView(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class LoginView extends StatefulWidget {
+  const LoginView({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<LoginView> createState() => _LoginViewState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _LoginViewState extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
@@ -61,47 +41,58 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return CupertinoPageScaffold(
+      child: Column(
+        children: <Widget>[
+          const Padding(
+              padding: EdgeInsets.only(top: 60.0),
+              child: Center(
+                  child: Text(
+                    'Musicly',
+                    style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),
+                  )
+              )
+          ),
+          // Create the password text box.
+          Padding(
+            padding: const EdgeInsets.only(
+                left: 40.0, right: 40.0, top: 55.0, bottom: 0),
+            child: CupertinoTextField(
+              placeholder: 'Password',
+              obscureText: true,
+              textAlign: TextAlign.center,
+              placeholderStyle: VanderbiltStyles.textRowPlaceholder,
+              style: VanderbiltStyles.textRowPlaceholder,
+              padding: const EdgeInsets.only(
+                  left: 0.0, right: 0.0, top: 15.0, bottom: 15.0),
+              decoration: BoxDecoration(
+                color: CupertinoColors.white,
+                borderRadius: BorderRadius.circular(25.0),
+              ),
+              onChanged: (text) {
+              },
+              onSubmitted: (text) {
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          // Create the login button.
+          Padding(
+            padding: const EdgeInsets.only(
+                left: 30.0, right: 30.0, top: 25.0, bottom: 0),
+            child: CupertinoButton(
+              child: const Text(
+                'Login',
+                style: VanderbiltStyles.textButton,
+              ),
+              onPressed: () {
+              },
+              borderRadius: BorderRadius.circular(25.0),
+              color: VanderbiltStyles.gold,
+              pressedOpacity: 0.75,
             ),
-          ],
-        ),
+          )
+        ]
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
